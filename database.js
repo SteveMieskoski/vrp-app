046fc3e8063126cd340c45e14f99b45e163e0c8f
Steve Mieskoski <Stevemieskoski@gmail.com>
2017-10-09 13:28:52 -0400

    Update database.js

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 // MongoDB Configuration configuration (Change this URL to your own DB)
 if(process.env.MONGODB_URI){
+	console.log(process.env.MONGODB_URI);
 	mongoose.connect(process.env.MONGODB_URI);
 } else {
 	mongoose.connect("mongodb://localhost/vrp-app");
